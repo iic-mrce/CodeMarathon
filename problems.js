03a1e39f0a4bff694eb68712098b05cf9952ee77
Harsh RB <harshrb2424@gmail.com>
2024-09-08 17:31:06 +0530

    problems page

---
+++ problems.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    // Load JSON data from a file (or an API)
+    $.getJSON('./assets/round1.json', function (data) {
+      // Iterate through the JSON data and append rows to the table
+      $.each(data, function (index, item) {
+        $('#round1Table tbody').append(
+          '<tr><td>' + item.problem_no + '</td><td>' + item.problem + '</td></tr>'
+        );
+      });
+    });
+    $.getJSON('./assets/round2.json', function (data) {
+      // Iterate through the JSON data and append rows to the table
+      $.each(data, function (index, item) {
+        $('#round2Table tbody').append(
+          '<tr><td>' + item.problem_no + '</td><td>' + item.problem + '</td></tr>'
+        );
+      });
+    });
+    $.getJSON('./assets/round3.json', function (data) {
+      // Iterate through the JSON data and append rows to the table
+      $.each(data, function (index, item) {
+        $('#round3Table tbody').append(
+          '<tr><td>' + item.problem_no + '</td><td>' + item.problem + '</td></tr>'
+        );
+      });
+    });
+  });
